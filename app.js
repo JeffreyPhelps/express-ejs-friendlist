db61dd59a7ec9648bb0e346f199fea8ac1bc7df7
Jeffrey <phelpsjeffrey@yahoo.com>
2018-10-30 07:59:00 -0600

    Final Rough Draft

---
+++ app.js
@@ -0,0 +1,36 @@
+
+
+var express = require("express");
+var app = express();
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+let friends = ['Norman', 'David', 'Ezekiel', 'Chris', 'Vivian'];
+
+
+
+app.get("/", function(req, res){
+   res.render("index.ejs"); 
+});
+
+
+app.get("/friends", function(req, res){
+   res.render("friends.ejs", {friends: friends});
+});
+
+
+app.post("/addFriend", function(req, res){
+    let newFriend = req.body.newFriend;
+    friends.push(newFriend);
+    res.redirect("/friends");
+});
+
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("Server up and running on...Some Fn port!"); 
+});
+
+
